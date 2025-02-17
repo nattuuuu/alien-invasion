608917f335159a2794e4fd9553662b10299ed41a
nattuuuu <nsandcc@gmail.com>
2025-02-17 12:38:14 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,27 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "j":return hex`0078000408020100001c00010a006400f401640000040000000000000000000000000005000004520000000400012904000800012908000c0001270c00100002202410001400012018001c00012420002400012a2400280001272c003000011934003800012c38003c00012c3c004000011b430044000420292a2c`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
